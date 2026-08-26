0a0c8ca56e1fb69056e164e380ea66100497e0ff
Jeff <scubajeffrey@criptext.com>
2026-08-26 12:51:36 +0800

    show HDR info in TV's metadata popup

--- app/src/main/java/site/leos/apps/lespas/tv/TVSliderFragment.kt
+++ app/src/main/java/site/leos/apps/lespas/tv/TVSliderFragment.kt
@@ -147,6 +147,7 @@ class TVSliderFragment: Fragment() {
     private val wordIterator: BreakIterator = BreakIterator.getWordInstance(Locale.getDefault())
     private var captionHintingAnimation = AnimatorSet()
     private var isSortedByDate = true
+    private var isHDR = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -159,9 +160,12 @@ class TVSliderFragment: Fragment() {
             imageLoaderModel.getResourceRoot(),
             playerViewModel,
             { on -> },
-            { media, imageView, type -> if (type != NCShareViewModel.TYPE_NULL) imageLoaderModel.setImagePhoto(media, imageView!!, NCShareViewModel.TYPE_FULL) },
-            { media, imageView, plManager, panorama -> imageLoaderModel.setImagePhoto(media, imageView!!, NCShareViewModel.TYPE_PANORAMA, plManager, panorama) },
-            { view -> imageLoaderModel.cancelSetImagePhoto(view) },
+            { media, imageView, type -> if (type != NCShareViewModel.TYPE_NULL) imageLoaderModel.setImagePhoto(media, imageView!!, NCShareViewModel.TYPE_FULL) { isHDR = imageView.getTag(R.id.HDR_TAG) as? Boolean == true }},
+            { media, imageView, plManager, panorama -> imageLoaderModel.setImagePhoto(media, imageView!!, NCShareViewModel.TYPE_PANORAMA, plManager, panorama) { isHDR = false }},
+            { view ->
+                imageLoaderModel.cancelSetImagePhoto(view)
+                isHDR = false
+            },
             { delta ->
                 if (metaPage.isVisible) toggleMeta(NCShareViewModel.RemotePhoto(Photo(dateTaken = LocalDateTime.MIN, lastModified = LocalDateTime.MIN)), true)
                 else {
@@ -570,7 +574,10 @@ class TVSliderFragment: Fragment() {
                             pWidth = width
                             pHeight = height
                         }
-                        tvSize.text = if (pm.size == 0L) String.format("%sw × %sh", "$pWidth", "$pHeight") else String.format("%s, %s", Tools.humanReadableByteCountSI(pm.size), String.format("%sw × %sh", "$pWidth", "$pHeight"))
+                        var sizeString:String = if (pm.size != 0L) String.format("%s, ", Tools.humanReadableByteCountSI(pm.size)) else ""
+                        sizeString += String.format(Locale.getDefault(), "%dw × %dh", pWidth, pHeight)
+                        if (isHDR) sizeString += ", HDR"
+                        tvSize.text = sizeString
                         trSize.isVisible = true
 
                         if (pm.mfg.isNotEmpty()) {
